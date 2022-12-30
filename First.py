853cf3b11c34a0fba26c18ed37cf57f9305fd7ab
ShaheerKiani <101730692+ShaheerKiani@users.noreply.github.com>
2022-12-30 19:27:48 +0500

    Q5 Done

--- First.py
+++ First.py
@@ -24,3 +24,15 @@ SurfaceArea = (6*Length)**2
 
 print("The volume is", Volume)
 print("The Surface Area is", SurfaceArea)
+
+#Q5
+X1 = int(input("Enter x coordinate of point 1: "))
+Y1 = int(input("Enter y coordinate of point 1: "))
+
+X2 = int(input("Enter x coordinate of point 2: "))
+Y2 = int(input("Enter y coordinate of point 2: "))
+
+MidpointOfX = (X2+X1)/2
+MidpointOfY = (Y2+Y1)/2
+
+print(MidpointOfX,",", MidpointOfY)
